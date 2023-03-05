818c96e00c955de361f095d4161a82c12d910507
Emirhan KAKAR <emirkkr10@hotmail.com>
2023-03-05 04:59:24 +0300

    Add project files.

---
+++ Controllers/FormsController.cs
@@ -0,0 +1,72 @@
+﻿using LenaProject.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LenaProject.Controllers
+{
+    public class FormsController : Controller
+    {
+        LenaFormDBEntities db = new LenaFormDBEntities();
+
+        [Authorize]
+        public ActionResult FormList(Users user)
+        {
+            var formsList = db.Forms.Where(x => x.UserId == user.UserId);
+            return View(formsList);
+        }
+
+        [Authorize]
+        public ActionResult FormCreate()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult FormCreate(Forms form)
+        {
+            var userId = Session["userId"];
+
+            var user = db.Users.FirstOrDefault(x => x.UserId == (int)userId);
+
+            form.CreatedAt = DateTime.Now;
+            form.UserId = (int)userId;
+            db.Forms.Add(form);
+            db.SaveChanges();
+
+            return RedirectToAction("FormList", user);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("{controller}/forms{id}")]
+        public ActionResult FormDetails(int id)
+        {
+            var form = db.Forms.FirstOrDefault(x => x.FormId == id);
+            return View("FormDetails", form);
+        }
+
+        [Authorize]
+        public ActionResult FormEdit(int id)
+        {
+            var form = db.Forms.FirstOrDefault(x => x.FormId == id);
+
+            return View(form);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult FormEdit(Forms form)
+        {
+            var frm = db.Forms.FirstOrDefault(x => x.FormId == form.FormId);
+            var user = db.Users.FirstOrDefault(x => x.UserId == form.UserId);
+
+            db.Forms.Remove(frm);
+            db.Forms.Add(form);
+            db.SaveChanges();
+
+            return RedirectToAction("FormList", user);
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using LenaProject.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LenaProject.Controllers
+{
+
+    public class LoginController : Controller
+    {
+        LenaFormDBEntities db = new LenaFormDBEntities();
+
+        // GET: Login
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Users user)
+        {
+            var userObj = db.Users.FirstOrDefault(x => x.Username == user.Username && x.Password == user.Password);
+
+            if (userObj != null)
+            {
+                Session["userId"] = userObj.UserId;
+                FormsAuthentication.SetAuthCookie(user.Username, false);
+                return RedirectToAction("FormList", "Forms", userObj);
+            }
+            else
+            {
+                ViewBag.LoginError = "Hatalı Kullanıcı Adı veya Şifre";
+                return View();
+            }
+
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
